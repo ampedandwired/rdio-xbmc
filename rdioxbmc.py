18d1a24dfe890115043dab17f9393daaef0ca4c5
Charles Blaxland <charles.blaxland@gmail.com>
2012-05-27 20:30:58 +1000

    Moved RdioApi to its own file, and did some other project refactoring/cleanup.

--- rdioxbmc.py
+++ rdioxbmc.py
@@ -17,19 +17,14 @@
 import sys
 import os
 import inspect
-from urlparse import urlparse, parse_qs
 from t0mm0.common.addon import Addon
-from t0mm0.common.net import Net
-import CommonFunctions
 
 ADDON_ID = 'plugin.audio.rdio'
 addon = Addon(ADDON_ID, argv=sys.argv)
 sys.path.append(os.path.join(addon.get_path(), 'resources', 'lib'))
 
-from rdioapi import Rdio
-from rdiostream import get_rtmp_info
+from rdioapi import RdioApi
 
-RDIO_DOMAIN = 'localhost'
 
 class XbmcRdioOperation:
   
@@ -80,15 +75,10 @@ class XbmcRdioOperation:
     self._addon.end_of_directory()
 
   def play(self, **params):
-    track_id = params['key']
-    rtmp_info = get_rtmp_info(RDIO_DOMAIN, self._rdio_api.get_playback_token(), track_id)
-    stream_url = rtmp_info['rtmp']
-    for key, value in rtmp_info.items():
-      stream_url += '' if key == 'rtmp' else ' %s=%s' % (key, value)
-
-    self._addon.log_debug("Resolved playback URL to " + stream_url)
+    key = params['key']
+    stream_url = self._rdio_api.resolve_playback_url(key)
     self._addon.resolve_url(stream_url)
-    
+
   def settings(self):
     self._addon.show_settings()
     
@@ -100,68 +90,6 @@ class XbmcRdioOperation:
       handler(**self._addon.queries)
     else:
       handler()
-
-
-class RdioApi:
-  _STATE_FILE_NAME = 'rdio-state.json'
-  
-  def __init__(self, addon):
-    self._addon = addon
-    self._net = Net()
-    self._state = addon.load_data(self._STATE_FILE_NAME)
-    if not self._state:
-      addon.log_debug("Persistent auth state not loaded")
-      self._state = {'rdio_api': {'auth_state': {}}, 'playback_token': None}
-    else:
-      addon.log_debug("Loaded persistent auth state")
-
-    apikey = addon.get_setting('apikey')
-    addon.log_debug("Connecting to Rdio with apikey " + apikey)
-    self._rdio = Rdio(apikey, addon.get_setting('apisecret'), self._state['rdio_api'])
-    if not self._rdio.authenticated:
-      self._authenticate()
-  
-    addon.log_notice("Connected successfully to Rdio with apikey " + apikey)
-      
-  def _authenticate(self):
-    self._addon.log_notice("Authenticating to Rdio")
-    auth_url = self._rdio.begin_authentication('oob')
-    parsed_auth_url = urlparse(auth_url)
-    url_base = "%s://%s" % (parsed_auth_url.scheme, parsed_auth_url.netloc)
-
-    self._addon.log_notice("Authorizing OAuth token " + auth_url)
-    html = self._net.http_GET(auth_url).content
-    login_path = CommonFunctions.parseDOM(html, 'form', {'name': 'login'}, 'action')
-    if login_path:
-      login_url = url_base + login_path[0]
-      username = self._addon.get_setting('username')
-      password = self._addon.get_setting('password')
-      self._addon.log_notice("Logging in to Rdio as %s using URL %s" % (username, login_url))
-      html = self._net.http_POST(login_url, {'username': username, 'password': password}).content
-
-    oauth_token = CommonFunctions.parseDOM(html, 'input', {'name': 'oauth_token'}, 'value')[0]
-    verifier = CommonFunctions.parseDOM(html, 'input', {'name': 'verifier'}, 'value')[0]
-    
-    self._addon.log_notice("Approving oauth token %s with pin %s" % (oauth_token, verifier))
-    self._net.http_POST(auth_url, {'oath_token': oauth_token, 'verifier': verifier, 'approve': ''})
-    
-    self._addon.log_notice("Verifying OAuth token on Rdio API with pin " + verifier)
-    self._rdio.complete_authentication(verifier)
-
-    self._addon.log_notice("Getting playback token")
-    self._state['playback_token'] = self.call('getPlaybackToken', domain=RDIO_DOMAIN)
-    self._addon.log_notice("Got playback token: " + self._state['playback_token'])
-    
-    self._addon.save_data(self._STATE_FILE_NAME, self._state)
-    self._addon.log_notice("Successfully authenticated to Rdio")
-
-  def call(self, method, **args):
-    self._addon.log_debug("Executing Rdio API call '%s' with args %s" % (method, args))
-    return self._rdio.call(method, **args)
-    
-  def get_playback_token(self):
-    return self._state['playback_token']
-
 
 
 XbmcRdioOperation(addon).execute()
