a7aa8f7e5355ffedeef72368893fae38780d7a72
Charles Blaxland <charles.blaxland@gmail.com>
2013-06-10 11:46:32 +1000

    Fixed missing import and added license headers

--- phantomxbmc.js
+++ phantomxbmc.js
@@ -1,3 +1,19 @@
+// Copyright 2012 Charles Blaxland
+// This file is part of rdio-xbmc.
+//
+// rdio-xbmc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// rdio-xbmc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with rdio-xbmc.  If not, see <http://www.gnu.org/licenses/>.
+
 var args = require('system').args;
 var script = require(args[1]);
 if (args.length > 2) {
--- rdio.js
+++ rdio.js
@@ -1,3 +1,19 @@
+// Copyright 2012 Charles Blaxland
+// This file is part of rdio-xbmc.
+//
+// rdio-xbmc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// rdio-xbmc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with rdio-xbmc.  If not, see <http://www.gnu.org/licenses/>.
+
 var page = new WebPage();
 page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31';
 
